d133277a55ccea588e06d4f0295e209a766d7f81
adnangemthing <53823301+adnangemthing@users.noreply.github.com>
2020-01-05 19:13:39 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -29,8 +29,8 @@ client.on('message', msg => {  //WESO#0001
   
 
   client.on('message', msg => {  //WESO#0001
-    if (msg.content === 'احبك') {  //WESO#0001
-      msg.reply('**حبك برص يا سافل**');  //WESO#0001
+    if (msg.content === 'عدنان') {  //WESO#0001
+      msg.reply('**adnan_gemthing#4668**');  //WESO#0001
     }
   });
 
